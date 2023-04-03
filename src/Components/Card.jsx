28261edc71779fd6d7af98d31ea503ec183e7280
StiMolG <smolinag@correo.iue.edu.co>
2023-04-02 19:10:20 -0500

    changes in card and footer

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -16,7 +16,7 @@ const Card = () => {
           {/* Na linha seguinte o link deverá utilizar a matricula, nome e sobrenome do dentista
           que vem da API */}
           <a href={`/dentist/MatriculaDoDentista`}>
-            <h5 className={`card-title ${styles.title}`}>Nome e Sobrenome do dentista</h5>
+            <h5 className={`card-title ${styles.title}`}>Nombre del dentista</h5>
           </a>
         </div>
       </div>
